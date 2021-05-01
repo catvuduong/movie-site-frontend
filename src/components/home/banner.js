f15bff3bb065f2b4bcab05fcdc12b473550326c1
Cát Vũ Dương <duongcatvu17@gmail.com>
2021-05-01 21:17:08 +0700

    Update banner.js

--- src/components/home/banner.js
+++ src/components/home/banner.js
@@ -88,7 +88,7 @@ export default class Banner extends Component {
                         </div>
                     </div>
                 </div>
-                {/* fskdlfjsasdsadsa sdsasDSAfsdfdsfdsfsdfsdfsd*/}
+                {/* fskdlfjsasdsadsa sdfdsd*/}
 
             </section>
         )
