9face4d8982f7e37e91aca1fb85dc09be48f895d
Cát Vũ Dương <duongcatvu17@gmail.com>
2021-05-01 20:49:30 +0700

    update

--- src/components/home/banner.js
+++ src/components/home/banner.js
@@ -88,6 +88,7 @@ export default class Banner extends Component {
                         </div>
                     </div>
                 </div>
+                {/* fskdlfjs */}
 
             </section>
         )
