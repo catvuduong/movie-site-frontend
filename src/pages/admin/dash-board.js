1a17eb695d285c78d3f8d2dc6159fa85be3edc37
Cát Vũ Dương <duongcatvu17@gmail.com>
2021-05-30 01:45:23 +0700

    update interface at dashboard page.
    update interface at dashboard page.

--- src/pages/admin/dash-board.js
+++ src/pages/admin/dash-board.js
@@ -1,22 +1,17 @@
 import React, { Component } from 'react';
-import Collapse from 'react-bootstrap/Collapse'
 import MovieManagement from '../../components/admin/movie-management';
 import UserManagement from '../../components/admin/user-management';
 import BranchManagement from '../../components/admin/branch-management';
 import TheaterManagement from '../../components/admin/theater-management';
-
-
-
 
 export default class DashBoard extends Component {
     constructor(props) {
         super(props);
         this.state = {
             toggleClicked: false,
+            activeBranch: false, activeTheaeter: false, activeMovie: false, activeUser: false,
+            activeShowtime: false, activeArticle: false,
             component: TheaterManagement,
-            openTheaterCollapse: false,
-            openMovieCollapse: false,
-            openUserCollapse: false,
         }
     }
 
@@ -30,18 +25,61 @@ export default class DashBoard extends Component {
             component
         })
     }
-    checkOpenCollapse = collapse => {
-        if (collapse) {
-            return "down"
+
+    setActiveClass = sign => {
+        switch (sign) {
+            case "branch": {
+                this.setState({
+                    activeBranch: true, activeTheaeter: false, activeMovie: false, activeUser: false, activeShowtime: false, activeArticle: false
+                })
+                break;
+            }
+            case "theater": {
+                this.setState({
+                    activeBranch: false, activeTheaeter: true, activeMovie: false, activeUser: false, activeShowtime: false, activeArticle: false
+                })
+                break;
+            }
+            case "movie": {
+                this.setState({
+                    activeBranch: false, activeTheaeter: false, activeMovie: true, activeUser: false, activeShowtime: false, activeArticle: false
+                })
+                break;
+            }
+            case "showtime": {
+                this.setState({
+                    activeBranch: false, activeTheaeter: false, activeMovie: false, activeUser: false, activeShowtime: true, activeArticle: false
+                })
+                break;
+            }
+            case "article": {
+                this.setState({
+                    activeBranch: false, activeTheaeter: false, activeMovie: false, activeUser: false, activeShowtime: false, activeArticle: true
+                })
+                break;
+            }
+            case "user": {
+                this.setState({
+                    activeBranch: false, activeTheaeter: false, activeMovie: false, activeUser: true, activeShowtime: false, activeArticle: false
+                })
+                break;
+            }
+            default:
+                break;
         }
-        return "right"
     }
 
     renderHTML = Component => (<Component></Component>)
 
     render() {
-        let { openBranchCollapse, openTheaterCollapse, openMovieCollapse, openUserCollapse, toggleClicked, component } = this.state;
+        let { toggleClicked, component } = this.state;
         let toggleClass = toggleClicked ? "toggled" : "";
+        let activeBranch = this.state.activeBranch ? "active_button" : "";
+        let activeTheaeter = this.state.activeTheaeter ? "active_button" : "";
+        let activeMovie = this.state.activeMovie ? "active_button" : "";
+        let activeUser = this.state.activeUser ? "active_button" : "";
+        let activeShowtime = this.state.activeShowtime ? "active_button" : "";
+        let activeArticle = this.state.activeArticle ? "active_button" : "";
         return (
             <div className="myDashboard text-left">
                 <div className={`d-flex ${toggleClass}`} id="wrapper">
@@ -51,56 +89,41 @@ export default class DashBoard extends Component {
                         </div>
                         <div className="list-group list-group-flush dashboard_sidebar">
                             <div className="dashboard_item">
-                                <button className="list-group-item list-group-item-action bg-light " aria-controls="branch-collapse"
-                                    aria-expanded={openBranchCollapse}
-                                    onClick={() => this.setState({ openBranchCollapse: !openBranchCollapse })}>Branch
-                                         <i className={`fa fa-caret-${this.checkOpenCollapse(openBranchCollapse)} ml-5`}></i>
+                                <button className={`list-group-item list-group-item-action ${activeBranch}`}
+                                    onClick={() => { this.handleComponent(BranchManagement); this.setActiveClass("branch") }}>
+                                    <i className="fab fa-cloudsmith"></i><span>Branch</span>
                                 </button>
-                                <Collapse in={openBranchCollapse}>
-                                    <div id="branch-collapse" onClick={() => { this.handleComponent(BranchManagement) }}>
-                                        <div className="dashboard_getlist">
-                                            GET BRANCH LIST
-                                        </div>
-                                    </div>
-                                </Collapse>
                             </div>
                             <div className="dashboard_item">
-                                <button className="list-group-item list-group-item-action bg-light " aria-controls="theater-collapse"
-                                    aria-expanded={openTheaterCollapse}
-                                    onClick={() => this.setState({ openTheaterCollapse: !openTheaterCollapse })}>Theater
-                                         <i className={`fa fa-caret-${this.checkOpenCollapse(openTheaterCollapse)} ml-5`}></i>
+                                <button className={`list-group-item list-group-item-action ${activeTheaeter}`}
+                                    onClick={() => { this.handleComponent(TheaterManagement); this.setActiveClass("theater") }}>
+                                    <i className="fa fa-theater-masks"></i><span>Theater</span>
                                 </button>
-                                <Collapse in={openTheaterCollapse}>
-                                    <div id="branch-theater" onClick={() => { this.handleComponent(TheaterManagement) }}>
-                                        <div className="dashboard_getlist">
-                                            GET THEATER LIST
-                                        </div>
-                                    </div>
-                                </Collapse>
+
                             </div>
                             <div className="dashboard_item">
-                                <button className="list-group-item list-group-item-action bg-light " aria-controls="movie-collapse"
-                                    aria-expanded={openMovieCollapse}
-                                    onClick={() => this.setState({ openMovieCollapse: !openMovieCollapse })}>Movie
-                                         <i className={`fa fa-caret-${this.checkOpenCollapse(openMovieCollapse)} ml-5`}></i>
+                                <button className={`list-group-item list-group-item-action ${activeMovie}`}
+                                    onClick={() => { this.handleComponent(MovieManagement); this.setActiveClass("movie") }}>
+                                    <i className="fa fa-film"></i><span>Movie</span>
                                 </button>
-                                <Collapse in={openMovieCollapse}>
-                                    <div id="movie-collapse">
-                                        <div className="dashboard_getlist" onClick={() => { this.handleComponent(MovieManagement) }}>GET LIST MOVIE</div>
-                                    </div>
-                                </Collapse>
                             </div>
                             <div className="dashboard_item">
-                                <button className="list-group-item list-group-item-action bg-light dashboard_item" aria-controls="user-collapse"
-                                    aria-expanded={openUserCollapse}
-                                    onClick={() => this.setState({ openUserCollapse: !openUserCollapse })}>User
-                                   <i className={`fa fa-caret-${this.checkOpenCollapse(openUserCollapse)} ml-5`}></i>
+                                <button className={`list-group-item list-group-item-action ${activeShowtime}`}
+                                    onClick={() => { this.handleComponent(MovieManagement); this.setActiveClass("showtime") }}>
+                                    <i className="fa fa-clock"></i><span>Showtime</span>
                                 </button>
-                                <Collapse in={openUserCollapse}>
-                                    <div id="user-collapse">
-                                        <div className="dashboard_getlist" onClick={() => { this.handleComponent(UserManagement) }}>GET LIST USER</div>
-                                    </div>
-                                </Collapse>
+                            </div>
+                            <div className="dashboard_item">
+                                <button className={`list-group-item list-group-item-action ${activeArticle}`}
+                                    onClick={() => { this.handleComponent(MovieManagement); this.setActiveClass("article") }}>
+                                    <i className="fa fa-newspaper"></i><span>Article</span>
+                                </button>
+                            </div>
+                            <div className="dashboard_item">
+                                <button className={`list-group-item list-group-item-action ${activeUser}`}
+                                    onClick={() => { this.handleComponent(UserManagement); this.setActiveClass("user") }}>
+                                    <i className="fa fa-user"></i><span>User</span>
+                                </button>
                             </div>
                         </div>
                     </div>
