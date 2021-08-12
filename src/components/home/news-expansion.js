ae2362cb431d4e5561d8c7430f1dbca37a8e431e
Cát Vũ Dương <duongcatvu17@gmail.com>
2021-08-13 00:09:01 +0700

    update interface at article component.

--- src/components/home/news-expansion.js
+++ src/components/home/news-expansion.js
@@ -5,30 +5,35 @@ export default class NewsExpansion extends Component {
     render() {
         let firstList = this.props.listArticles.slice(0, 2);
         let secondList = this.props.listArticles.slice(2, 4);
+        let thirdList = this.props.listArticles.slice(4);
         return (
             <div className="article_content">
                 <div className="container">
                     <div className="row first_list">
-                        {
-                            firstList.map((item, index) => (
-                                <div className="col-6 article_item" key={index}>
-                                    <img src={"http://localhost:5000" + item.thumbnail} alt="" />
-                                    <h5>{item.title}</h5>
-                                    <p>{item.content}</p>
-                                </div>
-                            ))
-                        }
+                        {firstList.map((item, index) => (
+                            <div className="col-6 article_item" key={index}>
+                                <img src={"http://localhost:5000" + item.thumbnail} alt="" />
+                                <h5>{item.title}</h5>
+                                <p>{item.content}</p>
+                            </div>
+                        ))}
                     </div>
                     <div className="row second_list">
-                        {
-                            secondList.map((item, index) => (
-                                <div className="col-4 article_item" key={index}>
+                        {secondList.map((item, index) => (
+                            <div className="col-4 article_item" key={index}>
+                                <img src={"https://localhost:5001" + item.thumbnail} alt="" />
+                                <h5>{item.title}</h5>
+                                <p>{item.content}</p>
+                            </div>
+                        ))}
+                        <div className="col-4 article_item thirdList">
+                            {thirdList.map((item, index) => (
+                                <div className="thirdList_item" key={index}>
                                     <img src={"https://localhost:5001" + item.thumbnail} alt="" />
                                     <h5>{item.title}</h5>
-                                    <p>{item.content}</p>
                                 </div>
-                            ))
-                        }
+                            ))}
+                        </div>
                     </div>
                 </div>
             </div>
