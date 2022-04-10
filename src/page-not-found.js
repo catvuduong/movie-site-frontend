6a6055026e8dbacef8fd51a9587ad91b179ce835
Cát Vũ Dương <57439802+catvuduong@users.noreply.github.com>
2022-04-10 12:15:08 +0700

    edit page not found component.

--- src/page-not-found.js
+++ src/page-not-found.js
@@ -4,192 +4,21 @@ export default class PageNotFound extends Component {
 
     render() {
         return (
-            <div className='page-not-found'>
-                <div className="scenery">
-                    <div className="stars">
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
-                        <div className="star" />
+            <div className='notfound'>
+                <div className='notfound-main'>
+                    <div className='notfound-warning'>
+                        <h1 className='notfound-heading'>Oops!</h1>
                     </div>
-                    <div className="planet planet1" />
-                    <div className="planet planet2" />
-                    <div className="planet planet3" />
-                    <div className="girl">
-                        <div className="head" />
-                        <div className="body" />
-                        <div className="legs" />
-                    </div>
-                    <div className="title">
-                        <h1>
-                            <span>4</span>
-                            <div className="square">
-                                <div className="light light1" />
-                                <div className="light light2" />
-                                <div className="light light3" />
-                                <div className="light light4" />
-                                <div className="shadow shadow1" />
-                                <div className="shadow shadow2" />
-                                <ul className="stairs1">
-                                    <li />
-                                    <li />
-                                    <li />
-                                    <li />
-                                    <li />
-                                    <li />
-                                    <li />
-                                    <li />
-                                    <li />
-                                    <li />
-                                    <li />
-                                    <li />
-                                </ul>
-                                <ul className="stairs2">
-                                    <li />
-                                    <li />
-                                    <li />
-                                    <li />
-                                    <li />
-                                    <li />
-                                    <li />
-                                    <li />
-                                    <li />
-                                    <li />
-                                    <li />
-                                </ul>
-                                <ul className="stairs3">
-                                    <li />
-                                    <li />
-                                    <li />
-                                    <li />
-                                    <li />
-                                    <li />
-                                    <li />
-                                    <li />
-                                    <li />
-                                    <li />
-                                </ul>
-                                <ul className="stairs4">
-                                    <li />
-                                    <li />
-                                    <li />
-                                    <li />
-                                    <li />
-                                    <li />
-                                    <li />
-                                    <li />
-                                    <li />
-                                    <li />
-                                    <li />
-                                </ul>
-                            </div>
-                            <span>4</span>
-                        </h1>
-                    </div>
-                    <div className="message">
-                        <h2>Oops! Looks like you got lost</h2>
-                    </div>
-                    <div className="action">
-                        <button onClick={() => { this.props.history.push('/'); }}>Take me back</button>
+                    <div className='notfound-content'>
+                        <h4 className='notfound-title'>404 - PAGE NOT FOUND</h4>
+                        <p className='notfound-text'>The page you are looking for might have been removed had its name changed or is temporarily unavailable.</p>
+                        <button className='notfound-goback'
+                            onClick={() => { this.props.history.push('/'); }}
+                        >GO TO HOME PAGE</button>
                     </div>
                 </div>
             </div>
         )
     }
 }
+
