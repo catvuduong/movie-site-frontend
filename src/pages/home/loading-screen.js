090013643feb4d884521d09fbca062e36f4857fb
Cát Vũ Dương <57439802+catvuduong@users.noreply.github.com>
2022-04-10 22:19:08 +0700

    update loading screen.

--- src/pages/home/loading-screen.js
+++ src/pages/home/loading-screen.js
@@ -3,16 +3,10 @@ import React, { Component } from 'react'
 export default class LoadingScreen extends Component {
     render() {
         return (
-            <section className="myLoadingScreen" >
-                <div id="preloader" style={{ backgroundImage: `url(/images/backapp.jpg)` }}>
-                    <div className="loader">
-                        <span className="box"></span>
-                        <span className="box"></span>
-                        <div className="code">
-                            <img src="/images/web-logo.png" width="120px" alt="" />
-                        </div>
-                        <span className="txt">LOADING</span>
-                    </div>
+            <section className="loading" >
+                <div className='loading-screen' id="preloader">
+                    <img className='loading-image' src="./images/web-logo.png" alt="" />
+                    <h3 className=''>Loading</h3>
                 </div>
             </section>
         )
